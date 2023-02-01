5ebb4f80d42f01a45aeb4249e2ff4e8c3dcaee5f
Gerem66 <geremy.lecaplain66@gmail.com>
2023-02-01 02:49:55 -0330

    Cleaning

--- sql.php
+++ sql.php
@@ -4,36 +4,50 @@
 
     class DataBase
     {
-        /**
-         * @var mysqli $conn
-         */
-        private $conn;
+        /** @var mysqli $conn */
+        private $conn = null;
+
         private $db_hostname;
-        private $db_name;
+        private $db_database;
         private $db_username;
         private $db_password;
 
-        public function __construct($init_conn = true) {
-            list(
-                $this->db_hostname,
-                $this->db_name,
-                $this->db_username,
-                $this->db_password
-            ) = GetCredentials();
-            if ($init_conn) {
+        public function __construct($openConnection = true) {
+            if (!file_exists('settings.json')) {
+                return;
+            }
+
+            $settings = json_decode(file_get_contents('settings.json'));
+            $this->db_hostname = $settings->hostname;
+            $this->db_database = $settings->database;
+            $this->db_username = $settings->username;
+            $this->db_password = $settings->password;
+
+            if ($openConnection) {
                 $this->OpenConnection();
             }
         }
 
         public function __destruct() {
+            if ($this->conn === null) return;
+
             $this->conn->close();
+            $this->conn = null;
         }
 
-        private function OpenConnection() {
-            $this->conn = new mysqli($this->db_hostname, $this->db_username, $this->db_password, $this->db_name);
+        /**
+         * Open a connection to the database.
+         * @param bool $exitOnFail If the script should exit if the connection failed.
+         * @return bool If the connection was successful.
+         * @throws Exception if the connection is already open.
+         */
+        private function OpenConnection(&$error = null) {
+            $this->conn = new mysqli($this->db_hostname, $this->db_username, $this->db_password, $this->db_database);
             if ($this->conn->connect_error) {
-                die('Connection failed: ' . $this->conn->connect_error);
+                $error = $this->conn->connect_error;
+                return false;
             }
+            return true;
         }
 
         function IsSafe($string) {
@@ -97,6 +111,10 @@
             return $output;
         }
 
+        /**
+         * Get the tables in the database.
+         * @return array|false Array of tables or false if the query failed.
+         */
         public function GetTables() {
             $tables = null;
             $query = $this->conn->query('SHOW TABLES');
@@ -106,9 +124,9 @@
             while ($row = $query->fetch_assoc()) {
                 array_push($tables, $row);
             }
-            if ($tables === null) return null;
+            if ($tables === null) return false;
 
-            $tableMap = fn($table) => $table["Tables_in_{$this->db_name}"];
+            $tableMap = fn($table) => $table["Tables_in_{$this->db_database}"];
             return array_map($tableMap, $tables);
         }
 
@@ -117,18 +135,25 @@
         }
 
         /**
-         * @param int $accountID 0 if not connected
-         * @param int $deviceID
-         * @param string $type
-         * @param string $data
-         * @return bool Success of operation
+         * Return IP address of the client or "UNKNOWN" if failed
+         * @link https://stackoverflow.com/questions/3003145/how-to-get-the-client-ip-address-in-php
+         * @return string|false
          */
-        public function AddLog($accountID, $deviceID, $type, $data = null) {
-            $IP = GetClientIP();
-            $command = 'INSERT INTO TABLE (`AccountID`, `DeviceID`, `IP`, `Type`, `Data`) VALUES (?, ?, ?, ?, ?)';
-            $args = [ $accountID, $deviceID, $IP, $type, $data ];
-            $result = $this->QueryPrepare('Logs', $command, 'iisss', $args);
-            return $result !== false;
+        function GetClientIP() {
+            $keys = array(
+                'REMOTE_ADDR',
+                'HTTP_FORWARDED',
+                'HTTP_CLIENT_IP',
+                'HTTP_FORWARDED_FOR',
+                'HTTP_X_FORWARDED_FOR',
+                'HTTP_X_FORWARDED'
+            );
+            foreach ($keys as $k) {
+                if (!empty($_SERVER[$k])) {
+                    return $_SERVER[$k];
+                }
+            }
+            return false;
         }
     }
 
